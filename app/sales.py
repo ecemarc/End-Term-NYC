8f36c6008a069f6fd1f22e58019d3776d7ddf01b
ecemarc <ece.marcelli@stern.nyu.edu>
2020-06-25 16:15:48 -0400

    ZIP AND TC WORKING

--- app/sales.py
+++ app/sales.py
@@ -1,9 +1,12 @@
 # TEAM BEST
 import pandas as pd
 import os
-
+import numpy as np
+pd.options.mode.chained_assignment = None  # default='warn'
 
 # utility function to convert float or integer to usd-formatted string (for printing)
+
+
 def to_usd(my_price):
     # return "${0:,.2f}".format(my_price)
     return f"${my_price:,.2f}"
@@ -19,12 +22,21 @@ def do_query(csv_file):
     # INFO INPUTS
     #
     # ********************************eventually we will change to use csv_file that is given to this function*****************
-    CSV_FILENAME = "rollingsales.csv"
+    CSV_FILENAME = "rollingsales1.csv"
 
     csv_filepath = os.path.join(os.path.dirname(
         __file__), "..",  "data", CSV_FILENAME)
 
     df = pd.read_csv(csv_filepath)
+
+    # df = df.replace('NaN', np.nan)
+
+    df[" SALE PRICE "] = pd.to_numeric(
+        df[" SALE PRICE "], errors='coerce')
+    df["GROSS SQUARE FEET"] = pd.to_numeric(
+        df["GROSS SQUARE FEET"], errors='coerce')
+    df["SALE_PRICE"] = df[" SALE PRICE "]
+    df["GSF"] = df["GROSS SQUARE FEET"]
 
     sales = df.to_dict("record")
 
@@ -34,22 +46,10 @@ def do_query(csv_file):
         sales_price = (row[" SALE PRICE "])
         sq = (row["GROSS SQUARE FEET"])
         valid_zip.append(row["ZIP CODE"])
+    print(type(row["GSF"]))
 
-    # print(type(zip))
-    # print(type(row["ZIP CODE"]))
-    # print(type(row["TAX CLASS AT PRESENT"]))
+    # print(df.iloc[0])
 
-    if int(sq) != 0 and float(sales_price) != 0:
-        # calculating price for sq feet
-        price_per_sq = float(sales_price)/int(sq)
-    else:
-        print("PRICE PER SQUARE FOOT NOT AVAILABLE")
-
-    print(df.iloc[0])
-
-    # def sorted_by_neighbourhood(a):
-    #     return a["neighbourhood"]
-    nan_value = float("NaN")
     selected_zips = []
     selected_tc = []
     selected_items_sales_price = []
@@ -63,17 +63,19 @@ def do_query(csv_file):
                 if int(user_input_zip) in df["ZIP CODE"]:
                     print(df[df["ZIP CODE"] == int(user_input_zip)])
                     selected_items = df[df["ZIP CODE"] == int(user_input_zip)]
-                    selected_items[" SALE PRICE "] = pd.to_numeric(
-                        selected_items[" SALE PRICE "], errors='coerce')
-                    selected_items["GROSS SQUARE FEET"] = pd.to_numeric(
-                        selected_items["GROSS SQUARE FEET"], errors='coerce')
-                    print(selected_items.dtypes)
-                    selected_items_sf_price = selected_items_sales[" SALE PRICE "] / \
-                        selected_items_feet["GROSS SQUARE FEET"]
+                    selected_items = selected_items.dropna(
+                        subset=['GSF'])
+                    selected_items = selected_items.dropna(
+                        subset=["SALE_PRICE"])
+                    selected_items_new = selected_items.loc[(
+                        selected_items.SALE_PRICE >= float(10)) & (selected_items.GSF >= float(10))]
+                    print(selected_items_new)
+                    selected_items_sf_price = selected_items_new["SALE_PRICE"] / \
+                        selected_items_new["GSF"]
                     user_input_calc = input(
-                        "Would you like to learn square foot pricing for this zip? : ")
+                        "Would you like to learn square foot pricing for this ZIP? : ")
                     if user_input_calc == "YES" or user_input_calc == "yes":
-                        print(selected_items_sf_price)
+                        print(selected_items_sf_price.mean())
                     else:
                         break
 
@@ -86,6 +88,23 @@ def do_query(csv_file):
                     "Please enter the Tax Class Codes you are interested in: ")
                 if str(user_input_tc) in str(df["TAX CLASS AT PRESENT"]):
                     print(df[df["TAX CLASS AT PRESENT"] == str(user_input_tc)])
+                    selected_items_tc = df[df["TAX CLASS AT PRESENT"] == str(
+                        user_input_tc)]
+                    selected_items_tc = selected_items_tc.dropna(
+                        subset=['GSF'])
+                    selected_items = selected_items_tc.dropna(
+                        subset=["SALE_PRICE"])
+                    selected_items_tc_new = selected_items_tc.loc[(
+                        selected_items_tc.SALE_PRICE >= float(10)) & (selected_items_tc.GSF >= float(10))]
+                    print(selected_items_tc_new)
+                    selected_items_sf_price_tc = selected_items_tc_new["SALE_PRICE"] / \
+                        selected_items_tc_new["GSF"]
+                    user_input_calc = input(
+                        "Would you like to learn square foot pricing for this TAX cODE? : ")
+                    if user_input_calc == "YES" or user_input_calc == "yes":
+                        print(selected_items_sf_price_tc.mean())
+                    else:
+                        break
                 elif user_input == "DONE" or user_input == "done" or user_input == "Done":  # exiting when DONE
                     break
                 else:
